3f78a2d29be42dee7eb31bce87ce9b5a49df99de
FeatherP <featherp@qq.com>
2024-09-01 01:30:01 +0800

    简单测试一下Sqlite的创建，晚安捏

--- tests/sqlite-test1/sqlite-test1.cpp
+++ tests/sqlite-test1/sqlite-test1.cpp
@@ -2,7 +2,18 @@
 #include <sqlite3.h>
 
 int main(){
-    std::cout << "It is just a unfinished test pls.";
+    sqlite3 * database {};
+    int returnCode {};
+
+    returnCode = sqlite3_open("testdb.db", &database);
+
+    if (returnCode != 0){
+        std::cerr << "Can't open a sqlite file, error message is: " << sqlite3_errmsg(database) << std::endl;
+        return 1;
+    }
+    std::cout << "sqlite file opened successfully.\n";
+
+    sqlite3_close(database);
 
     return 0;
 }
